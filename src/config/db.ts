e7f4219e280feb7d720fdd3afa8ba4be41670ee0
mohammadreza8410 <mohammadrezagharravi2002@gmail.com>
2025-09-08 17:41:17 +0330

    fix: add DB retry and logging, improve JWT auth with role-based checks

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,31 @@
 import mongoose from "mongoose";
 
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 2000;
+
 export const connectDB = async (uri: string) => {
-  try {
-    mongoose.set("strictQuery", true);
-    await mongoose.connect(uri);
-    console.log("✅ MongoDB connected");
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err);
-    process.exit(1);
+  if (!uri) {
+    throw new Error("MONGO_URI is not defined in .env");
+  }
+  mongoose.set("strictQuery", true);
+
+  let attempts = 0;
+  while (attempts < MAX_RETRIES) {
+    try {
+      await mongoose.connect(uri, {});
+      console.log("✅ MongoDB connected");
+      return;
+    } catch (err) {
+      attempts++;
+      console.error(`mongodb connection attempt : ${attempts} failed  :`, err);
+
+      if (attempts < MAX_RETRIES) {
+        console.log(`retrying in ${RETRY_DELAY / 1000}s...`);
+        await new Promise((res) => setTimeout(res, RETRY_DELAY));
+      } else {
+        console.error("culd not connect to mongoDB afet maximum retries. ");
+        process.exit(1);
+      }
+    }
   }
 };
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,14 @@
 import jwt from "jsonwebtoken";
+import { ApolloError } from "apollo-server-express";
 import { User, IUser } from "../models/User";
+import { error } from "console";
 
 const JWT_SECRET = process.env.JWT_SECRET || "changeme";
+
+interface TokenPayload {
+  id: string;
+  role: string;
+}
 
 export const signToken = (user: IUser) => {
   return jwt.sign({ id: user.id.toString(), role: user.role }, JWT_SECRET, {
@@ -12,20 +19,18 @@ export const signToken = (user: IUser) => {
 export const getUserFromToken = async (token?: string) => {
   if (!token) return null;
   try {
-    const decoded = jwt.verify(token, JWT_SECRET) as {
-      id: string;
-      role: string;
-    };
+    const decoded = jwt.verify(token, JWT_SECRET) as TokenPayload;
     const user = await User.findById(decoded.id);
     return user;
   } catch {
+    console.error("invalid or expired token : ", error);
     return null;
   }
 };
 
 export const requireAuth = (user: IUser | null) => {
   if (!user) {
-    throw new Error("Unauthorized");
+    throw new ApolloError("Unauthorized: user not logged in or token invalid");
   }
   return user;
 };
